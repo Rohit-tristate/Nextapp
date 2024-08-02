190ee2a4e6d4f9f40555de481886908f3e1b7c35
Isha Pankhaniya <isha.pankhaniya@tristatetechnology.co.in>
2024-08-02 18:07:00 +0530

    adding new line

--- src/app/login/error.js
+++ src/app/login/error.js
@@ -4,6 +4,7 @@ export default function Error(){
     return (
         <div className="flex justify-center ">
             <p>Something went wrong</p>
+            <p>please try after sometime</p>
         </div>
     )
 }
