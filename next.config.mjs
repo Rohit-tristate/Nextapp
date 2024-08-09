56e82eb09ecf88f6439dbf581f62518b2747daf2
rohit thakur <rohit.thakur@tristatetechnology.co.in>
2024-08-09 18:00:02 +0530

    resolve conflict in next.confiq

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  env: {
-    GET_POSTLIST_URL: "http://localhost:3000/api/Getpost",
-    USER_LOGIN_URL: "http://localhost:3000/api/Login",
-    USER_REGISTRATION_URL: "http://localhost:3000/api/Register",
-    NEWPOST_URL: "http://localhost:3000/api/post",
-    GETDATA_BY_ID_URL: "http://localhost:3000/api/Getpost/post",
-    DELETE_POST_URL: "http://localhost:3000/api/deletepost",
-    ADMIN_LOGIN_URL: "http://localhost:3000/api/Admin/login",
-    ADD_ADMIN_URL: "http://localhost:3000/api/Admin/register"
-  },
-};
 
-export default nextConfig;
-=======
+  
+
     env: {
       GET_POSTLIST_URL: "http://localhost:3000/api/Getpost",
       USER_LOGIN_URL: "http://localhost:3000/api/Login",
@@ -30,4 +18,3 @@ export default nextConfig;
   };
   
   export default nextConfig;
->>>>>>> dev
